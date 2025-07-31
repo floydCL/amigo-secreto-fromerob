040f8f33c3af681dfc4b66448274638585d273db
Franco Romero <fromerob@fen.uchile.cl>
2025-07-31 16:50:54 -0400

    feat: actualiza lógica en app.js
    Incluye 3 nuevas funciones, la primera función es para agregar los nombres ingresados a una lista , la segunda función permite ordenar los nombres ingresados en una lista de forma vertical y la tercera función le da funcionalidad al botón para sortear dando como resultado un nombre aleatorio.

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = []
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo === "") {
+        alert("Por favor, ingresa un nombre.");
+    } else {
+        listaNombres.push(nombreAmigo);
+        document.getElementById("amigo").value = "";
+        mostrarLista();
+    }
+}
+
+function mostrarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < listaNombres.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = listaNombres[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (listaNombres.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * listaNombres.length);
+    let amigoSorteado = listaNombres[indiceAleatorio];
+    let mensaje = document.getElementById("resultado");
+    mensaje.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+}
